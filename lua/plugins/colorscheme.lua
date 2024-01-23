7c54d2c2e22109fbf5f002935c888af2654b295e
youssef toaussi <yousseftaoussi894@gmail.com>
2024-01-23 20:49:42 +0100

    commit lazy vim files

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,18 @@
+-- return {
+--   --  "craftzdog/solarized-osaka.nvim",
+--   "morhetz/gruvbox",
+--   lazy = true,
+--   priority = 1000,
+--   opts = {
+--     --transparent = true,
+--   },
+-- }
+return {
+  "ellisonleao/gruvbox.nvim",
+  priority = 1000,
+  config = true,
+  opts = {
+    transparent_mode = true,
+    terminal_colors = true,
+  },
+}
